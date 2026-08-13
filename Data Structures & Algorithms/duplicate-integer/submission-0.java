723881fb531ffa2919478ce8ea01d40ca7aba1d0
Sitikanth_Sahoo <151992828+sitikanthsahoo@users.noreply.github.com>
2026-08-13 19:44:40 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        
+
+        Arrays.sort(nums);
+        
+        int count = 0;
+
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i] == nums[i+1]){
+                count++;
+            }
+            
+
+            if(count>=1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
